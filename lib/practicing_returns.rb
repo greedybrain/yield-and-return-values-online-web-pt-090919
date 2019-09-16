c7c3784bde3d7fa39964fa72a4919071fe6e5297
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 00:48:22 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 def hello(array)
   i = 0
+  new_arr = []
   while i < array.length
-    yield(array[i])
+    yield(new_arr << "Hi, #{ array[i] }")
     i += 1
   end
+  new_arr
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+binding.pry
+# hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
